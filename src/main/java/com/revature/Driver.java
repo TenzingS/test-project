972a0fb1fd5810179b85ce9abe39e15afeeef43d
tashizangbusherpa <thesherpatenzing@gmail.com>
2023-03-07 14:56:32 -0500

    test

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -5,7 +5,7 @@ public class Driver {
     public static void main(String[] args) {
 
         if(args.length > 0)
-            System.out.println("Hello world from " + args[0] + ".");
+            System.out.println("Hello world from " + args[0]);
         else
             System.out.println("Hello world!");
     }
